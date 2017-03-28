a4cd8d430d3034c1c759a49aac8879a201c9862e
UWAGUCHI Shinji <s.uwaguchi@gmail.com>
2017-03-29 00:03:26 +0900

    promiseを使って再実装

--- RasPI/receivemessage.js
+++ RasPI/receivemessage.js
@@ -1,5 +1,5 @@
-var fs = require("fs");
-var exec = require('child_process').exec;
+var fs = require("fs-promise");
+var exec = require('child-process-promise').exec;
 var AWS = require('aws-sdk');
 
 // キャプチャ画像ファイル名
@@ -29,51 +29,57 @@ var s3 = new AWS.S3({
 // キャプチャ用のコマンドライン
 var capturecmd = "fswebcam -q -r 1280x960 --font sans:40 " + imagefilepath;
 
-// メッセージ取得
+// リクエストメッセージ取得
+console.log("read request message");
+// s3からget
+params = {};
+params["Bucket"] = s3bucket;
+params["Key"] = requestmessagekey;
 
+s3.getObject(params).promise().then(function(data) {
+    // get完了
+    console.log("get request message end");
+
+    // データ取得
+
+
+}).catch(function(err) {
+    console.log(err);
+});
 
 // キャプチャ実行
-exec(capturecmd, function(err, stdout, stderr) {
-    if(err){
-        console.log(stderr);
-    }else{
-        // キャプチャ完了
-        console.log("capture end");
-        // キャプチャファイルを読み込み
-        fs.readFile(imagefilepath, function(err, buf) {
-            if(err) {
-                console.log(err);
-            } else {
-                // キャプチャファイル読み込み完了
-                console.log("read capture file end");
-                // s3にput
-                params = {};
-                params["Bucket"] = s3bucket;
-                params["Key"] = recentimagekey;
-                params["ContentType"] = "image/jpeg";
-                params["ACL"] = "public-read";
-                params["Body"] = buf;  
+exec(capturecmd).then(function(result) {
+    var stdout = result.stdout;
+    var stderr = result.stderr;
 
-                s3.putObject(params, function(err, data) {
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        // put完了
-                        console.log("put s3 end");
+    // キャプチャ完了
+    console.log("capture end");
+    // キャプチャファイルを読み込み
+    return fs.readFile(imagefilepath);
+}).then(function(buf) {
+    // キャプチャファイル読み込み完了
+    console.log("read capture file end");
 
-                        // 一時ファイル削除
-                        fs.unlink(imagefilepath, function(err) {
-                            if(err) {
-                                console.log(err);
-                            } else {
-                                console.log("tmp imagefile delete");
-                            }
-                        });
-                    }
-                });
-            }
-        });
-    }
+    // s3にput
+    params = {};
+    params["Bucket"] = s3bucket;
+    params["Key"] = recentimagekey;
+    params["ContentType"] = "image/jpeg";
+    params["ACL"] = "public-read";
+    params["Body"] = buf;
+    return s3.putObject(params).promise();
+}).then(function(data) {
+    // put完了
+    console.log("put s3 end");
+
+    // 一時ファイル削除
+    return fs.unlink(imagefilepath);
+}).then(function() {
+    // 削除完了
+    console.log("tmp imagefile delete");
+}).catch(function(err) {
+    // エラー発生
+    console.log(err);
 });
 
 console.log("cmdexec end");
