522b148c67c91d715d59a66a161019ae7bb7ea7b
Shinji Uwaguchi <s.uwaguchi@gmail.com>
2017-03-31 22:46:14 +0900

    受信処理が終わったら次の受信処理にいくように修正

--- RasPI/nodejs/SQSReceivetest.js
+++ RasPI/nodejs/SQSReceivetest.js
@@ -10,64 +10,67 @@ var sqs = new AWS.SQS({
     region: 'ap-northeast-1'
 });
 
-// パラメータセット
-var receiveparams = {
-    QueueUrl: request_que_url,
-    MaxNumberOfMessages: 1
+var receiveMessage = function() {
+    // パラメータセット
+    var receiveparams = {
+        QueueUrl: request_que_url,
+        MaxNumberOfMessages: 1
+    };
+
+    // メッセージ受信
+    sqs.receiveMessage(receiveparams).promise().then(function(data) {
+        // 完了
+        console.log("receive message end");
+        console.log(JSON.stringify(data));
+
+        if(typeof data.Messages != 'undefined') {
+            // メッセージが受信できた場合
+            var curMessageId = data.Messages[0].MessageId;
+            var curReceiptHandle = data.Messages[0].ReceiptHandle;
+            var curBody = data.Messages[0].Body;
+
+            // ここで受信したメッセージの内容に従って処理
+
+
+
+
+            // ここでレスポンスを返す
+            // レスポンスキューにメッセージを送信
+            // メッセージ本体
+            var responsemessage = {raspiresponse: 'OK', requestMessageId: curMessageId};
+
+            // パラメータセット
+            var responseparams = {
+                QueueUrl: response_que_url,
+                MessageBody: JSON.stringify( responsemessage )
+            };
+
+            // レスポンスメッセージ送信
+            sqs.sendMessage(responseparams).promise().then(function(data) {
+                // 送信完了
+                console.log("send response message end");
+            }).catch(function(err) {
+                // エラー発生
+                console.log(err);
+            });
+
+            // 処理が終わったらメッセージ削除
+            var deleteparams = {
+                QueueUrl: request_que_url,
+                ReceiptHandle: data.Messages[0].ReceiptHandle
+            };
+            return sqs.deleteMessage(deleteparams).promise();
+        }
+    }).then(function(data) {
+        // 削除完了したら次のメッセージ受信処理へ
+        console.log("end");
+        receiveMessage();
+    }).catch(function(err) {
+        // エラー発生
+        console.log(err);
+    });
 };
 
-// メッセージ受信
-sqs.receiveMessage(receiveparams).promise().then(function(data) {
-    // 完了
-    console.log("receive message end");
-    console.log(JSON.stringify(data));
-
-    if(typeof data.Messages != 'undefined') {
-        // メッセージが受信できた場合
-        var curMessageId = data.Messages[0].MessageId;
-        var curReceiptHandle = data.Messages[0].ReceiptHandle;
-        var curBody = data.Messages[0].Body;
-
-        // ここで受信したメッセージの内容に従って処理
-
-
-
-
-        // ここでレスポンスを返す
-        // レスポンスキューにメッセージを送信
-        // メッセージ本体
-        var responsemessage = {raspiresponse: 'OK', requestMessageId: curMessageId};
-
-        // パラメータセット
-        var responseparams = {
-            QueueUrl: response_que_url,
-            MessageBody: JSON.stringify( responsemessage )
-        };
-
-        // レスポンスメッセージ送信
-        sqs.sendMessage(responseparams).promise().then(function(data) {
-            // 送信完了
-            console.log("send response message end");
-        }).catch(function(err) {
-            // エラー発生
-            console.log(err);
-        });
-
-        // 処理が終わったらメッセージ削除
-        var deleteparams = {
-            QueueUrl: request_que_url,
-            ReceiptHandle: data.Messages[0].ReceiptHandle
-        };
-        return sqs.deleteMessage(deleteparams).promise();
-    }
-}).then(function(data) {
-    // 削除完了
-    console.log("end");
-}).catch(function(err) {
-    // エラー発生
-    console.log(err);
-});
-
-
-
+// メッセージ受信スタート
+receiveMessage();
 
