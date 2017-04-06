92a3a9433d8dea0ab37958d95a16a5d610784bf5
Shinji Uwaguchi <s.uwaguchi@gmail.com>
2017-04-06 23:35:09 +0900

    レスポンス受信処理をリファクタリング

--- RasPI/nodejs/SQSSendtest.js
+++ RasPI/nodejs/SQSSendtest.js
@@ -39,43 +39,70 @@ sqs.sendMessage(params).promise().then(function(data) {
 // パラメータセット
 var receiveparams = {
     QueueUrl: response_que_url,
-    MaxNumberOfMessages: 1
+    MaxNumberOfMessages: 10
 };
 
 // メッセージ受信
-sqs.receiveMessage(receiveparams).promise().then(function(data) {
-    // 完了
-    console.log("receive response message end");
-    console.log(JSON.stringify(data));
+var maxreceivecount = 10;
+var receivecount = 0;
+var receiveend = false;
 
-    if(typeof data.Messages != 'undefined') {
-        // メッセージが受信できた場合
-        var curReceiptHandle = data.Messages[0].ReceiptHandle;
-        var curBody = JSON.parse(data.Messages[0].Body);
+var receiveMessage = function() {
+    sqs.receiveMessage(receiveparams).promise().then(function(data) {
+        // 完了
+        console.log("receive response message end");
+        console.log(JSON.stringify(data));
 
-        if(curBody.requestMessageId === requestid) {
-            // 自身が送ったメッセージへのレスポンスの場合
-            console.log("my receive response message found");
+        if(typeof data.Messages != 'undefined') {
+            // メッセージが受信できた場合
+            for( var i = 0; i < data.Messages.length; i++ ) {
+                var curReceiptHandle = data.Messages[i].ReceiptHandle;
+                var curBody = JSON.parse(data.Messages[i].Body);
+
+                if(curBody.requestMessageId === requestid) {
+                    // 自身が送ったメッセージへのレスポンスの場合
+                    console.log("my receive response message found");
 
 
 
+                    // 受信処理完了フラグ
+                    receiveend = true;
 
-            // 処理が終わったらメッセージ削除
-            var deleteparams = {
-                QueueUrl: response_que_url,
-                ReceiptHandle: curReceiptHandle
-            };
-            return sqs.deleteMessage(deleteparams).promise();
+                    // 処理が終わったらメッセージ削除
+                    var deleteparams = {
+                        QueueUrl: response_que_url,
+                        ReceiptHandle: curReceiptHandle
+                    };
+                    // 処理できた時点でリターン
+                    return sqs.deleteMessage(deleteparams).promise();
+                }
+                console.log("get next message");
+            }
+        } else {
+            // レスポンスがない場合はエラー扱い
+            console.log("no response");
         }
-    } else {
-        // レスポンスがない場合はエラー扱い
-        console.log("no response");
-    }
-}).then(function(data) {
-    // 削除完了
-    console.log("end");
-}).catch(function(err) {
-    // エラー発生
-    console.log(err);
-});
+    }).then(function(data) {
+        // 1リクエストの処理完了
+        console.log("request end");
+
+        // 受信処理が完了してない場合はリトライ
+        if( receiveend == false ) {
+            // リクエスト数カウンタインクリメント
+            receivecount++;
+            // 最大値までいっていなければ再度受信処理へ
+            if( receivecount < maxreceivecount ) {
+                receiveMessage();
+            }
+        }
+    }).catch(function(err) {
+        // エラー発生
+        console.log(err);
+    });
+    console.log("----------end");
+}
+
+// レスポンス受信処理実行
+receiveMessage();
+
 
