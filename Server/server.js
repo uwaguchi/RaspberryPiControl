2d8b73efa7090c2e7cac2ca9f3f97fe51aeb776a
Shinji Uwaguchi <s.uwaguchi@gmail.com>
2017-04-18 23:07:11 +0900

    queryを取得できるように修正

--- Server/server.js
+++ Server/server.js
@@ -5,8 +5,10 @@ function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
+    var query = url.parse(request.url, true).query;
+    console.log("query: " + JSON.stringify(query));
 
-    route(handle, pathname, response);
+    route(handle, pathname, query, response);
   }
 
   http.createServer(onRequest).listen(8888);
