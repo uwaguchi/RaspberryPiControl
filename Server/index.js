de36b65404af0bdcffc51a4800fc064a0b07856e
Shinji Uwaguchi <s.uwaguchi@gmail.com>
2017-04-28 13:45:23 +0900

    ラスパイへメッセージを送るAPIを追加

--- Server/index.js
+++ Server/index.js
@@ -5,6 +5,6 @@ var requestHandlers = require("./requestHandlers");
 var handle = {}
 handle["/raspicontrol"] = requestHandlers.raspicontrol;
 handle["/api/raspimessage/request"] = requestHandlers.api_raspimessage_request;
-handle["/api/raspimessage/response"] = requestHandlers.api_raspimessage_response;
+handle["/api/raspimessage/capture"] = requestHandlers.api_raspimessage_capture;
 
 server.start(router.route, handle);
