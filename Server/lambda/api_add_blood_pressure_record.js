9b28b42f55389a23d5ab147ed13622acef479405
Shinji Uwaguchi <s.uwaguchi@gmail.com>
2020-06-04 23:49:53 +0900

    血圧データレコード追加API新規追加（まだ途中）

---
+++ Server/lambda/api_add_blood_pressure_record.js
@@ -0,0 +1,17 @@
+var send2Raspi = require('./sendMessage2Raspi');
+
+exports.handler = function(event, context, callback) {
+  // パラメータ取得
+
+
+  // S3からのデータ読み込み
+
+
+  // データ追加
+
+
+  // S3のデータを更新
+
+
+};
+
